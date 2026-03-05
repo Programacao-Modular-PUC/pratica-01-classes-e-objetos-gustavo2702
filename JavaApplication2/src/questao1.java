f24c065beb741c2c340b94bb1abd25a2c8b54c43
751513 <751513@PMG34INFLB20717.PUCMINAS.NET>
2026-03-05 18:37:18 -0300

    commit teste

---
+++ JavaApplication2/src/questao1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 751513
+ */
+import java.util.Scanner;
+public class questao1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        print
+    }
+}
