7e9b9e65e29315334947f54167d325f4dd52449d
751513 <751513@PMG34INFLB20717.PUCMINAS.NET>
2026-03-05 18:52:38 -0300

    teste3

--- JavaApplication2/src/questao1.java
+++ JavaApplication2/src/questao1.java
@@ -12,11 +12,10 @@ public class questao1 {
     public static void main(String[] args){
         int c1,c2;
         Scanner sc = new Scanner(System.in);
-        trianguloret t= new trianguloret();
+        trianguloret t = new trianguloret();
         System.out.println("Digite o cateto 1 e o cateto 2");
         t.c1 = sc.nextInt();
         t.c2 = sc.nextInt();
-        
     }
 
     private static class trianguloret {
